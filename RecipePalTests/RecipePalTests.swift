d7ca7a99c2a792256207e47d45655e7e8f3c1fcc
vanya <vanya.moo@gmail.com>
2025-09-17 10:31:38 +0100

    Initial Commit

---
+++ RecipePalTests/RecipePalTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipePalTests.swift
+//  RecipePalTests
+//
+//  Created by Vanya Mutafchieva on 17/09/2025.
+//
+
+import Testing
+@testable import RecipePal
+
+struct RecipePalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
