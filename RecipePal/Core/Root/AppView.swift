10bb3150d130bad8f1a91b1af526f942bc8ae4e4
vanya <vanya.moo@gmail.com>
2025-09-18 11:31:59 +0100

    [Feature] Add AppView

--- RecipePal/Core/Root/AppView.swift
+++ RecipePal/Core/Root/AppView.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
+// onboarding - signed out
+// tabbar - signed in
+
 struct AppView: View {
+    
+    @State private var showTabBar: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if showTabBar {
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    Text("TabBar")
+                }
+                .transition(.move(edge: .trailing))
+            } else {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Onboarding")
+                }
+                .transition(.move(edge: .leading))
+            }
+        }
+        .animation(.smooth, value: showTabBar)
+        .onTapGesture {
+            showTabBar.toggle()
+        }
     }
 }
 
