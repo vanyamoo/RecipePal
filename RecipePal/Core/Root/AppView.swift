aecbbd64d668798407e39726f74d104ba92e4004
vanya <vanya.moo@gmail.com>
2025-09-18 11:29:07 +0100

    [Patch] Fix Launch Screen to LaunchScreen

---
+++ RecipePal/Core/Root/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 17/09/2025.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppView()
+}
