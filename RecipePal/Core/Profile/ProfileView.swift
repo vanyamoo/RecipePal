7f577ee1ab4bc888ebc10fcfb6d784d31ffdb748
vanya <vanya.moo@gmail.com>
2025-09-19 15:21:01 +0100

    [Feature] Add Settings button and SettingsView sheet to ProfileView

--- RecipePal/Core/Profile/ProfileView.swift
+++ RecipePal/Core/Profile/ProfileView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State private var showSettingsView: Bool = false
+    
     var body: some View {
         NavigationStack {
             Text("Profile")
                 .navigationTitle("Profile")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        settingsButton
+                    }
+                }
+        }
+        .sheet(isPresented: $showSettingsView) {
+            Text("SettingsView")
         }
     }
+    
+    private var settingsButton: some View {
+        Button {
+            onSettingsButtonTapped()
+        } label: {
+            Image(systemName: "gear")
+                .font(.headline)
+        }
+    }
+    
+    private func onSettingsButtonTapped() {
+        showSettingsView = true
+    }
+    
 }
 
 #Preview {
