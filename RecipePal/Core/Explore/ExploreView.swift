08d0a91309016feba78f804095edde4d6de2007f
vanya <vanya.moo@gmail.com>
2025-09-19 14:15:34 +0100

    [Feature] Add Explore folder and ExploreView

---
+++ RecipePal/Core/Explore/ExploreView.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 19/09/2025.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Explore")
+                .navigationTitle("Explore")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
