fe373538dbcdd98849e69b0aef4606e3c4e58856
vanya <vanya.moo@gmail.com>
2025-09-19 15:31:21 +0100

    [Feature] Add Welcome folder and WelcomeView with NavigationStack to Core folder

---
+++ RecipePal/Core/Welcome/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 19/09/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Welcome")
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
