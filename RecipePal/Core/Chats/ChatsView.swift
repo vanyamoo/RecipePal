33fbe1dd45cc3edd21909e6a457d0ea57b106137
vanya <vanya.moo@gmail.com>
2025-09-19 14:24:38 +0100

    [Feature] Add Chats folder and ChatsView

---
+++ RecipePal/Core/Chats/ChatsView.swift
@@ -0,0 +1,21 @@
+//
+//  ChatsView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 19/09/2025.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Chats")
+                .navigationTitle("Chats")
+        }
+    }
+}
+
+#Preview {
+    ChatsView()
+}
