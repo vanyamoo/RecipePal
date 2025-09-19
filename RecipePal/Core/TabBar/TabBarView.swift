4b9299597f53a122aff273b370b8acc374414248
vanya <vanya.moo@gmail.com>
2025-09-19 12:06:37 +0100

    [Feature] Change deployment target to iOS18

---
+++ RecipePal/Core/TabBar/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 19/09/2025.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
