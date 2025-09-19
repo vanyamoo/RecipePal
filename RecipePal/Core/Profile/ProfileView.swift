9cd5bc1a6a35e6f28ff61db290c8121b44d60cad
vanya <vanya.moo@gmail.com>
2025-09-19 14:26:56 +0100

    [Feature] Add Profile folder and ProfileView

---
+++ RecipePal/Core/Profile/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 19/09/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Profile")
+                .navigationTitle("Profile")
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
