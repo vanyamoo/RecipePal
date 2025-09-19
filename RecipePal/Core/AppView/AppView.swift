d374abe159bb33a0a614d6d44abc9616beb406df
vanya <vanya.moo@gmail.com>
2025-09-19 15:34:01 +0100

    [Patch] Update AppView to display TabBarView and WelcomeView

--- RecipePal/Core/AppView/AppView.swift
+++ RecipePal/Core/AppView/AppView.swift
@@ -16,16 +16,10 @@ struct AppView: View {
         AppViewBuilder(
             showTabBar: showTabBar,
             tabBarView: {
-                ZStack {
-                    Color.red.ignoresSafeArea()
-                    Text("TabBar")
-                }
+                TabBarView()
             },
             onboardingView: {
-                ZStack {
-                    Color.blue.ignoresSafeArea()
-                    Text("Onboarding")
-                }
+                WelcomeView()
             }
         )
     }
