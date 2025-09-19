43893c922c12c50c827b290ec673cae45b7e21f4
vanya <vanya.moo@gmail.com>
2025-09-19 14:55:15 +0100

    [Feature] Update TabBarView with the three new custom tab content views

--- RecipePal/Core/TabBar/TabBarView.swift
+++ RecipePal/Core/TabBar/TabBarView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct TabBarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Tab("Explore", systemImage: "eyes") {
+                ExploreView()
+            }
+            
+            Tab("Chats", systemImage: "bubble.left.and.bubble.right.fill") {
+                ChatsView()
+            }
+            
+            Tab("Profile", systemImage: "person.fill") {
+                ProfileView()
+            }
+        }
     }
 }
 
