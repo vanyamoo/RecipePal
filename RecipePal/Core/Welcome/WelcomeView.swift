db86f162528b7ebd8a001984711280987bb06920
vanya <vanya.moo@gmail.com>
2025-09-22 11:22:15 +0100

    [Feature] Add "Get Started" NavigationLink to bottom of WelcomeView screen

--- RecipePal/Core/Welcome/WelcomeView.swift
+++ RecipePal/Core/Welcome/WelcomeView.swift
@@ -10,7 +10,25 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         NavigationStack {
-            Text("Welcome")
+            VStack {
+                
+                Text("Welcome")
+                    .frame(maxHeight: .infinity)
+                
+                NavigationLink {
+                    OnboardingCompletedView()
+                } label: {
+                    Text("Get Started")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .background(.accent)
+                        .cornerRadius(16)
+                }
+
+            }
+            .padding(16)
         }
     }
 }
