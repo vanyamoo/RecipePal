b93867c3a6dc7fbda644736d5a0475b3395517f0
vanya <vanya.moo@gmail.com>
2025-09-22 10:55:41 +0100

    [Feature] Update App Icon

---
+++ RecipePal/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingCompletedView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 22/09/2025.
+//
+
+import SwiftUI
+
+struct OnboardingCompletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingCompletedView()
+}
