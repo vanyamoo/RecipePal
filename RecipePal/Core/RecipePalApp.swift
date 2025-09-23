1848a9493ea4cd7efbee6e5e2fa05dcc591434c4
vanya <vanya.moo@gmail.com>
2025-09-23 10:28:42 +0100

    [Clean] Move RecipePalApp to Core folder

---
+++ RecipePal/Core/RecipePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipePalApp.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 17/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
