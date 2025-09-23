f640728a478b2708c49f033d125eabbeb22e01c4
vanya <vanya.moo@gmail.com>
2025-09-23 11:35:43 +0100

    [Clean] Move AppState into its own file

---
+++ RecipePal/Core/AppView/AppState.swift
@@ -0,0 +1,40 @@
+//
+//  AppState.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 23/09/2025.
+//
+import SwiftUI
+
+@Observable
+class AppState {
+    private(set) var showTabBar: Bool {
+        didSet {
+            UserDefaults.showTabbarView = showTabBar
+        }
+    }
+    
+    init(showTabBar: Bool = UserDefaults.showTabbarView) {
+        self.showTabBar = showTabBar
+    }
+    
+    func updateViewState(showTabBarView: Bool) {
+        showTabBar = showTabBarView
+    }
+}
+
+extension UserDefaults {
+    
+    private struct Keys {
+        static let showTabbarView = "showTabbarView"
+    }
+    
+    static var showTabbarView: Bool {
+        get {
+            standard.bool(forKey: Keys.showTabbarView)
+        }
+        set {
+            standard.set(newValue, forKey: Keys.showTabbarView)
+        }
+    }
+}
--- RecipePal/Core/AppView/AppView.swift
+++ RecipePal/Core/AppView/AppView.swift
@@ -7,39 +7,6 @@
 
 import SwiftUI
 
-@Observable
-class AppState {
-    private(set) var showTabBar: Bool {
-        didSet {
-            UserDefaults.showTabbarView = showTabBar
-        }
-    }
-    
-    init(showTabBar: Bool = UserDefaults.showTabbarView) {
-        self.showTabBar = showTabBar
-    }
-    
-    func updateViewState(showTabBarView: Bool) {
-        showTabBar = showTabBarView
-    }
-}
-
-extension UserDefaults {
-    
-    private struct Keys {
-        static let showTabbarView = "showTabbarView"
-    }
-    
-    static var showTabbarView: Bool {
-        get {
-            standard.bool(forKey: Keys.showTabbarView)
-        }
-        set {
-            standard.set(newValue, forKey: Keys.showTabbarView)
-        }
-    }
-}
-
 struct AppView: View {
     
     @State var appState: AppState = AppState()
