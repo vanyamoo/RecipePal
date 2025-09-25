22d4d4ba8dc855243bc84c27f33bf800eb29aae7
vanya <vanya.moo@gmail.com>
2025-09-25 10:47:56 +0100

    [Feature] Add IntroView folder, add OnboardingIntroView file

---
+++ RecipePal/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,50 @@
+//
+//  OnboardingIntroView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 25/09/2025.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Make your own ")
+                +
+                Text("cooking assistants ")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.accent)
+                +
+                Text("and chat with them!\n\nHave ")
+                +
+                Text("real conversations ")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.accent)
+                +
+                Text("with AI generated responses.")
+            }
+            .frame(maxHeight: .infinity)
+            .baselineOffset(6)
+            .padding(24)
+            
+            ctaButton
+        }
+        .padding(24)
+        .font(.title3)
+    }
+    
+    private var ctaButton: some View {
+        NavigationLink {
+            //OnboardingThemeView()
+        } label: {
+            Text("Continue")
+                .callToActionButton()
+        }
+    }
+}
+
+#Preview {
+    OnboardingIntroView()
+}
