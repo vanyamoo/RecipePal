20d33fd4b4a6237ad6a0466722fed68cb5dddca6
vanya <vanya.moo@gmail.com>
2025-09-26 11:52:10 +0100

    [Feature] Add new folder Services (to RecipePal folder), add folder RecipeAssistant, add folder Models, add `RecipeAssistantModel` file

---
+++ RecipePal/Services/RecipeAssistant/Models/RecipeAssistantModel.swift
@@ -0,0 +1,81 @@
+//
+//  RecipeAssistantModel.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 26/09/2025.
+//
+
+import Foundation
+
+struct RecipeAssistantModel {
+    let id: String
+    let name: String?
+    let characterOption: Cathegory?
+    let cookingTime: CookingTime?
+    let cuisine: Cuisine?
+    let mainIngredient: MainIngredient?
+    let profileImageName: String?
+    let authorId: String?
+    let dateCreated: Date?
+    
+    init(
+        id: String,
+        name: String? = nil,
+        characterOption: Cathegory? = nil,
+        cookingTime: CookingTime? = nil,
+        cuisine: Cuisine? = nil,
+        mainIngredient: MainIngredient? = nil,
+        profileImageName: String? = nil,
+        authorId: String? = nil,
+        dateCreated: Date? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.characterOption = characterOption
+        self.cookingTime = cookingTime
+        self.cuisine = cuisine
+        self.mainIngredient = mainIngredient
+        self.profileImageName = profileImageName
+        self.authorId = authorId
+        self.dateCreated = dateCreated
+    }
+}
+
+enum Cathegory: String {
+    case traybakeDinner = "Traybake Dinner"
+    case healthyBowl = "Healthy Bowl"
+    case quickAndEasy = "Quick and Easy"
+    case soup = "Soup"
+    case afternoonSnack = "Afternoon Snack"
+    case cookingWithKids = "Cooking with Kids"
+    case pasta = "Pasta"
+}
+
+enum CookingTime: Int {
+    case fiveMinutes = 5
+    case tenMinutes = 10
+    case fifteenMinutes = 15
+    case twentyMinutes = 20
+    case thirtyMinutes = 30
+    case fortyFiveMinutes = 45
+    case oneHour = 60
+    case twoHours = 120
+}
+
+enum Cuisine: String {
+    case italian = "Italian"
+    case indian = "Indian"
+    case japanese = "Japanese"
+    case asian = "Asian"
+    
+}
+
+enum MainIngredient: String {
+    case chicken = "Chicken"
+    case beef = "Beef"
+    case pork = "Pork"
+    case fishAndSeafood = "Fish and Seafood"
+    case vegetables = "Vegetables"
+    case grainsAndLegumes = "Grains and Legumes"
+    case pasta = "Pasta"
+}
