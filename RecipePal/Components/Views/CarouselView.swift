d35f639c47582fd456d88250eb133dc6d2f08096
vanya <vanya.moo@gmail.com>
2025-09-29 13:27:21 +0100

    [Feature] Add page control (little dots) underneath carousel

--- RecipePal/Components/Views/CarouselView.swift
+++ RecipePal/Components/Views/CarouselView.swift
@@ -12,26 +12,36 @@ struct CarouselView: View {
     var items: [RecipeAssistantModel] = RecipeAssistantModel.mocks
     
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 0) {
+        VStack {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(items, id: \.self) { item in
+                        HeroCellView(
+                            title: item.name,
+                            subtitle: item.description,
+                            imageName: item.profileImageName
+                        )
+                        .containerRelativeFrame(.horizontal) // Makes the view take up the full width
+                        .scrollTransition(.interactive.threshold(.visible(0.95)), transition: { content, phase in // main effect logic
+                            content
+                                .scaleEffect(phase.isIdentity ? 1 : 0.9)
+                        })
+                    }
+                }
+                .scrollTargetLayout() // Essential: Defines the layout items for paging
+            }
+            .frame(height: 200)
+            .scrollIndicators(.hidden)
+            .scrollTargetBehavior(.paging) // Forces the ScrollView to snap from one item to the next
+            
+            HStack {
                 ForEach(items, id: \.self) { item in
-                    HeroCellView(
-                        title: item.name,
-                        subtitle: item.description,
-                        imageName: item.profileImageName
-                    )
-                    .containerRelativeFrame(.horizontal) // Makes the view take up the full width
-                    .scrollTransition(.interactive.threshold(.visible(0.95)), transition: { content, phase in // main effect logic
-                        content
-                            .scaleEffect(phase.isIdentity ? 1 : 0.9)
-                    })
+                    Circle()
+                        .fill(.secondary.opacity(0.3))
+                        .frame(width: 8)
                 }
             }
-            .scrollTargetLayout() // Essential: Defines the layout items for paging
         }
-        .frame(height: 200)
-        .scrollIndicators(.hidden)
-        .scrollTargetBehavior(.paging) // Forces the ScrollView to snap from one item to the next
     }
 }
 
