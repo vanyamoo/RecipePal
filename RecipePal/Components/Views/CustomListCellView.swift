927ca0476e496a685d83dc2e0d85cf2313cf7533
vanya <vanya.moo@gmail.com>
2025-10-01 09:24:20 +0100

    Add `CustomListCellView` to Components/Views

--- RecipePal/Components/Views/CustomListCellView.swift
+++ RecipePal/Components/Views/CustomListCellView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct CustomListCellView: View {
+    
+    var imageName: String? = Constants.randomImage
+    var title: String? = "Playdate ideas"
+    var subtitle: String? = "Recipe for a snack that takes 15 min"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ZStack {
+                if let imageName {
+                    ImageLoaderView(urlString: imageName)
+                        
+                } else {
+                    Rectangle()
+                        .fill(Color.secondary.opacity(0.4))
+                }
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .frame(height: 60)
+            .cornerRadius(16)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                if let title {
+                    Text(title)
+                        .font(.headline)
+                }
+                
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.subheadline)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(12)
+        .padding(.vertical, 4)
+        .background(Color(.systemBackground))
     }
 }
 
 #Preview {
-    CustomListCellView()
+    ZStack {
+        Color.gray.ignoresSafeArea()
+        
+        VStack {
+            CustomListCellView()
+            CustomListCellView(title: nil)
+            CustomListCellView(imageName: nil)
+            CustomListCellView(subtitle: nil)
+        }
+    }
 }
