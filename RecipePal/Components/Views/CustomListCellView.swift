43a891d39d6a587abb6a5da9557fa1bf2a56a8ce
vanya <vanya.moo@gmail.com>
2025-10-01 08:55:06 +0100

    Fix bug in `ImageLoaderView` making opacity 0.001

---
+++ RecipePal/Components/Views/CustomListCellView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListCellView.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 01/10/2025.
+//
+
+import SwiftUI
+
+struct CustomListCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListCellView()
+}
