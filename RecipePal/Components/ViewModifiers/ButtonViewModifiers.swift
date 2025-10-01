e516f0dec4eda8f49fec6652142efeb3c33f2d2a
vanya <vanya.moo@gmail.com>
2025-10-01 10:54:40 +0100

    Add `struct HightlightButtonStyle` to ButtonViewModifiers file, add `func highlightButtonStyle(action:)` to View extension

---
+++ RecipePal/Components/ViewModifiers/ButtonViewModifiers.swift
@@ -0,0 +1,58 @@
+//
+//  ButtonViewModifiers.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 01/10/2025.
+//
+
+import SwiftUI
+
+struct HightlightButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .overlay {
+                configuration.isPressed ? Color.accent.opacity(0.3) : Color.accent.opacity(0)
+            }
+            .animation(.smooth, value: configuration.isPressed)
+    }
+}
+
+extension View {
+    func highlightButtonStyle(action: @escaping () -> Void) -> some View {
+        Button {
+            action()
+        } label: {
+            self
+        }
+        .buttonStyle(HightlightButtonStyle())
+    }
+}
+
+#Preview {
+    
+    VStack(spacing: 24) {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .tappableBackground()
+            .highlightButtonStyle {
+                // action
+            }
+        
+        Button {
+            // action
+        } label: {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        
+        Button {
+            // action
+        } label: {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .callToActionButton()
+        }
+    }
+    .padding(24)
+}
---
+++ RecipePal/Components/ViewModifiers/View+EXT.swift
@@ -0,0 +1,46 @@
+//
+//  View+EXT.swift
+//  RecipePal
+//
+//  Created by Vanya Mutafchieva on 22/09/2025.
+//
+
+import SwiftUI
+
+extension View {
+    func callToActionButton() -> some View {
+        self
+            .frame(maxWidth: .infinity)
+            .frame(height: 55)
+            .font(.headline)
+            .foregroundStyle(.white)
+            .background(.accent)
+            .cornerRadius(16)
+    }
+    
+    func tappableBackground() -> some View {
+        background(.black.opacity(0.001))
+    }
+    
+    func removeListRowFormatting() -> some View {
+        self
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            .listRowBackground(Color.clear)
+    }
+    
+    func addingGradientBackgroundForText() -> some View {
+        background(
+            LinearGradient(
+                gradient: Gradient(
+                    colors: [
+                        .black.opacity(0),
+                        .black.opacity(0.3),
+                        .black.opacity(0.6)
+                    ]
+                ),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        )
+    }
+}
