63a5bdc5478d798e0599b4161100c0083c71850a
vanya <vanya.moo@gmail.com>
2025-10-03 10:22:41 +0100

    Update `ChatsView` with some `ChatModel` data

--- RecipePal/Core/Chats/ChatsView.swift
+++ RecipePal/Core/Chats/ChatsView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ChatsView: View {
+    
+    @State private var chats: [ChatModel] = ChatModel.mocks
+    
     var body: some View {
         NavigationStack {
-            Text("Chats")
-                .navigationTitle("Chats")
+            List {
+                ForEach(chats) { chat in
+                    Text(chat.id)
+                }
+            }
+            .navigationTitle("Chats")
         }
     }
 }
