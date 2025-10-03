11b48927f1988a03629a6a7a385d186dc1131f3d
vanya <vanya.moo@gmail.com>
2025-10-03 08:13:53 +0100

    Create chat message data model

--- RecipePal/Services/Chat/Models/ChatMessageModel.swift
+++ RecipePal/Services/Chat/Models/ChatMessageModel.swift
@@ -5,3 +5,73 @@
 //  Created by Vanya Mutafchieva on 02/10/2025.
 //
 
+import Foundation
+
+struct ChatMessageModel: Identifiable {
+    let id: String
+    /// The parent chatId that will link to the ChatModel
+    let chatId: String
+    /// The ID of the user who wrote the message
+    let authorId: String?
+    let content: String?
+    let seenByIds: [String]?
+    let dateCreated: Date?
+    
+    init(
+        id: String,
+        chatId: String,
+        authorId: String? = nil,
+        content: String? = nil,
+        seenByIds: [String]? = nil,
+        dateCreated: Date? = nil
+    ) {
+        self.id = id
+        self.chatId = chatId
+        self.authorId = authorId
+        self.content = content
+        self.seenByIds = seenByIds
+        self.dateCreated = dateCreated
+    }
+    
+    static var mock: ChatMessageModel {
+        mocks[0]
+    }
+    
+    static var mocks: [ChatMessageModel] {
+        let now = Date()
+        return [
+            ChatMessageModel(
+                id: "msg1",
+                chatId: "1",
+                authorId: "user1",
+                content: "Hello, how are you?",
+                seenByIds: ["user2", "user3"],
+                dateCreated: now
+            ),
+            ChatMessageModel(
+                id: "msg2",
+                chatId: "2",
+                authorId: "user2",
+                content: "I'm doing well, thanks for asking!",
+                seenByIds: ["user1"],
+                dateCreated: now.addingTimeInterval(minutes: -5)
+            ),
+            ChatMessageModel(
+                id: "msg3",
+                chatId: "3",
+                authorId: "user3",
+                content: "Anyone up for a game tonight?",
+                seenByIds: ["user1", "user2", "user4"],
+                dateCreated: now.addingTimeInterval(hours: -1)
+            ),
+            ChatMessageModel(
+                id: "msg4",
+                chatId: "1",
+                authorId: "user1",
+                content: "Sure, count me in!",
+                seenByIds: nil,
+                dateCreated: now.addingTimeInterval(hours: -2, minutes: -15)
+            )
+        ]
+    }
+}
