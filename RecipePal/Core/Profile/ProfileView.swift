310e256ee72feca571b2a872c6295b3fec11ee38
vanya <vanya.moo@gmail.com>
2025-10-06 09:28:27 +0100

    Add `myRecipeAssistantsSection` in `ProfileView`

--- RecipePal/Core/Profile/ProfileView.swift
+++ RecipePal/Core/Profile/ProfileView.swift
@@ -10,29 +10,29 @@ import SwiftUI
 struct ProfileView: View {
     
     @State private var showSettingsView: Bool = false
+    @State private var showCreateRecipeAssistantView: Bool = false
     @State private var currentUser: UserModel? = .mock
+    @State private var myRecipeAssistants: [RecipeAssistantModel] = RecipeAssistantModel.mocks
     
     var body: some View {
         NavigationStack {
             List {
                 myInfoSection
                 
-                Section {
-                    
-                } header: {
-                    Text("My Avatars")
-                }
-
+                myRecipeAssistantsSection
             }
-                .navigationTitle("Profile")
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        settingsButton
-                    }
+            .navigationTitle("Profile")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    settingsButton
                 }
+            }
         }
         .sheet(isPresented: $showSettingsView) {
             SettingsView()
+        }
+        .fullScreenCover(isPresented: $showCreateRecipeAssistantView) {
+            Text("Create Recipe Assistant")
         }
     }
     
@@ -43,6 +43,37 @@ struct ProfileView: View {
                 .frame(width: 100, height: 100)
                 .frame(maxWidth: .infinity)
                 .removeListRowFormatting()
+        }
+    }
+    
+    private var myRecipeAssistantsSection: some View {
+        Section {
+            ForEach(myRecipeAssistants, id: \.self) { assistant in
+                CustomListCellView(
+                    imageName: assistant.profileImageName,
+                    title: assistant.name,
+                    subtitle: nil
+                )
+                .anyButton(.highlight) {
+                    
+                }
+                .removeListRowFormatting()
+            }
+            .onDelete { indexSet in
+                onDeleteAssistant(indexSet: indexSet)
+            }
+            
+        } header: {
+            HStack(spacing: 0) {
+                Text("My Recipe Assistants")
+                Spacer()
+                Image(systemName: "plus.circle.fill")
+                    .font(.title)
+                    .foregroundStyle(.accent)
+                    .anyButton {
+                        onNewAssistantButtonPressed()
+                    }
+            }
         }
     }
     
@@ -60,8 +91,17 @@ struct ProfileView: View {
         showSettingsView = true
     }
     
+    private func onNewAssistantButtonPressed() {
+        showCreateRecipeAssistantView = true
+    }
+    
+    private func onDeleteAssistant(indexSet: IndexSet) {
+        myRecipeAssistants.remove(atOffsets: indexSet)
+    }
+    
 }
 
 #Preview {
     ProfileView()
+        .environment(AppState())
 }
