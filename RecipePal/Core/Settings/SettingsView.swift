3eec144d1beb720a300d246ba618ccb5844a4253
vanya <vanya.moo@gmail.com>
2025-10-07 11:06:59 +0100

    Add three sections to `SettingsView` - `accountSection, purchasesSection, applicationSection`

--- RecipePal/Core/Settings/SettingsView.swift
+++ RecipePal/Core/Settings/SettingsView.swift
@@ -12,22 +12,111 @@ struct SettingsView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(AppState.self) private var appState
     
+    @State private var isPremium: Bool = false
+    @State private var isAnonymousUser: Bool = true
+    @State private var showCreateAccountView: Bool = false
+    
     var body: some View {
         NavigationStack {
             List {
-                Button {
-                    onSignOutButtonTapped()
-                } label: {
-                    Text("Sign Out")
-                }
+                accountSection
+                
+                purchasesSection
+                
+                applicationSection
             }
             .navigationTitle("Settings")
         }
     }
     
-    private func onSignOutButtonTapped() {
+    private var accountSection: some View {
+        Section {
+            if isAnonymousUser {
+                Text("Save & back-up account")
+                    .rowFormatting()
+                    .anyButton(.highlight) {
+                        onCreateAccountPressed()
+                    }
+                    .removeListRowFormatting()
+            } else {
+                Text("Sign Out")
+                    .rowFormatting()
+                    .anyButton(.highlight) {
+                        onSignOutPressed()
+                    }
+                    .removeListRowFormatting()
+            }
+            
+            Text("Delete Account")
+                .foregroundStyle(.red)
+                .rowFormatting()
+                .anyButton(.highlight) {
+                    onDeleteAccountPressed()
+                }
+                .removeListRowFormatting()
+        } header: {
+            Text("Account")
+        }
+    }
+    
+    private var purchasesSection: some View {
+        Section {
+            HStack {
+                Text("Account status: \(isPremium ? "PREMIUM" : "FREE")")
+                Spacer(minLength: 0)
+                if isPremium {
+                    Text("MANAGE")
+                        .badgeButton()
+                }
+            }
+            .rowFormatting()
+            .anyButton(.highlight) {
+                
+            }
+            .disabled(!isPremium)
+            .removeListRowFormatting()
+        } header: {
+            Text("Purchases")
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section {
+            HStack {
+                Text("Version")
+                Spacer(minLength: 0)
+                Text(Utilities.appVersion ?? "")
+                    .foregroundStyle(.secondary)
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+            
+            HStack {
+                Text("Build Number")
+                Spacer(minLength: 0)
+                Text(Utilities.buildNumber ?? "")
+                    .foregroundStyle(.secondary)
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+            
+            Text("Contact us")
+                .foregroundStyle(.blue)
+                .rowFormatting()
+                .anyButton(.highlight, action: {
+                    
+                })
+                .removeListRowFormatting()
+        } header: {
+            Text("Application")
+        } footer: {
+            Text("Created by Tiny Empire. \nLearn more at https://tinyempire.ie")
+                .baselineOffset(6)
+        }
+    }
+    
+    func onSignOutPressed() {
         // do some logic to sign user out of app!
-        
         dismiss()
         
         Task {
@@ -36,6 +125,13 @@ struct SettingsView: View {
         }
     }
     
+    func onDeleteAccountPressed() {
+        
+    }
+    
+    func onCreateAccountPressed() {
+        showCreateAccountView = true
+    }
 }
 
 fileprivate extension View {
