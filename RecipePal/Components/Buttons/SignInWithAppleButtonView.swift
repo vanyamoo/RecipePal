d4efae3f5715ebe43f0dba8366a677216f2d2645
vanya <vanya.moo@gmail.com>
2025-10-07 11:24:32 +0100

    Make AuthenticationServices import in SignInWithAppleButtonView `public`

--- RecipePal/Components/Buttons/SignInWithAppleButtonView.swift
+++ RecipePal/Components/Buttons/SignInWithAppleButtonView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import AuthenticationServices
+public import AuthenticationServices
 
 public struct SignInWithAppleButtonView: View {
     public let type: ASAuthorizationAppleIDButton.ButtonType
